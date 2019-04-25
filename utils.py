b7476eabe05784d9350f6cdd75df515fd367212e
Crispino <gabriel.crispino@hotmail.com>
2019-04-24 21:10:41 -0300

    Add use of scipy sparse matrices for better memory consumption

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy.sparse import csr_matrix
 import sys
 import datetime
 
@@ -8,18 +9,12 @@ def read_rewards(path):
     return pd.read_csv(path, header=None).values.T[0]
 
 
-"""
-    for each pair (action: file_path), execute read_action function
-    and reduce all to (n_states,n_actions,n_states) array
-"""
-
-
 def get_n_states(path):
     df = pd.read_csv(path, sep='   ', header=None, engine='python')
     return int(df[0].max())
 
 
-def read_actions1(paths_dict):
+def read_actions2(paths_dict):
     actions = paths_dict.keys()
     action_mats = {a: read_action(path) for a, path in paths_dict.items()}
     n_states = len(action_mats[actions[0]][0])
@@ -33,23 +28,30 @@ def read_actions1(paths_dict):
 
 
 def read_actions(paths):
+    """
+        for each pair (action: file_path), execute read_action function
+        and reduce all to (n_states,n_actions,n_states) array
+    """
     n_actions = len(paths)
 
-    n_states = get_n_states(paths[0])
+    action_mats = [read_action(paths[i]) for i in range(0, n_actions)]
+    n_states = len(action_mats[0][0])
 
-    action_mats = np.array([read_action(paths[i])
-                            for i in range(0, n_actions)], dtype="float")
+    res_mat = [[0] * n_actions] * n_states
 
-    return action_mats.transpose(1, 0, 2)
+    for i_s in range(0, n_states):
+        for a in range(0, n_actions):
+            res_mat[i_s][a] = action_mats[a][i_s]
+        res_mat[i_s] = csr_matrix(res_mat[i_s])
 
-
-"""
-    given path, read file on this location and
-    construct (n_actions,n_states,n_states)
-"""
+    return res_mat
 
 
 def read_action(path):
+    """
+        given path, read file on this location and
+        construct (n_actions,n_states,n_states)
+    """
     print("Reading action data from file ", path)
 
     df = pd.read_csv(path, sep='   ', header=None, engine='python')
@@ -63,16 +65,13 @@ def read_action(path):
             _s = int(row[1])
             p = row[2]
             a_mat[s - 1][_s - 1] = p
+    print("Finished ", path, "!")
 
     return a_mat
 
 
 def bellman(T, R, v, A, S, s, gamma):
     res = (float('-inf'), 0)
-
-    # print 'oi: ', T(0, A[0], None), R(0, None, None), v
-    # print T(0, A[0], None).shape, R(0, None, None).shape, v.shape
-    # sys.exit(0)
 
     for i in np.arange(0, len(A)):
         q = np.sum(
@@ -97,7 +96,7 @@ def evaluate(T, R, v, A, pi, S, gamma, epsilon):
         begin = datetime.datetime.now()
 
         for s in S:
-            v_new[s - 1] = T(s, A[pi[s - 1]], None).dot(
+            v_new[s - 1] = T(s, pi[s - 1], None).dot(
                 R(s, None, None) + gamma * v_old
             )
         # end = datetime.datetime.now()
@@ -108,6 +107,74 @@ def evaluate(T, R, v, A, pi, S, gamma, epsilon):
         # if (np.sum((np.fabs(v_old - v_new))) <= epsilon):
         if norm < epsilon:
             break
-        v_old = np.copy(v_new)
+        v_old = v_new
         i += 1
     return v_new
+
+
+base_arrow = {
+    'width': 0.04,
+    'head_width': 0.3,
+    'head_length': 0.2,
+    'color': "white"
+}
+
+arrows = {
+    'N': {
+        'x': 0,
+        'dx': 0,
+        'y': 0.4,
+        'dy': -0.6,
+        **base_arrow
+    },
+    'S': {
+        **base_arrow,
+        'x': 0,
+        'dx': 0,
+        'y': -0.4,
+        'dy': 0.6
+    },
+    'L': {
+        **base_arrow,
+        'x': -0.4,
+        'dx': 0.6,
+        'y': 0,
+        'dy': 0
+    },
+    'O': {
+        **base_arrow,
+        'x': 0.4,
+        'dx': -0.6,
+        'y': 0,
+        'dy': 0
+    },
+    'U': {
+        **base_arrow,
+        'x': 0,
+        'dx': 0,
+        'y': 0.1,
+        'dy': -0.001,
+        'head_width': 0.35,
+        'head_length': 0.25,
+    },
+    'D': {
+        **base_arrow,
+        'x': 0,
+        'dx': 0,
+        'y': -0.1,
+        'dy': 0,
+        'head_width': 0.35,
+        'head_length': 0.25,
+    }
+}
+
+
+def plotArrow(action, i, j, A, plt):
+    arrow = arrows[A[action]]
+    plt.arrow(
+        arrow['x'] + j,
+        arrow['y'] + i,
+        arrow['dx'], arrow['dy'],
+        width=arrow['width'],
+        head_width=arrow['head_width'], head_length=arrow['head_length'], color=arrow['color']
+    )
